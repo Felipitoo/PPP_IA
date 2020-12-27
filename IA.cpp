df46da39889df5136730b2e9e5ba741a7c7b39c3
Felipitoo <felipe.montero.14@sansano.usm.cl>
2020-12-27 19:53:46 -0300

    ordenar capacidades

--- IA.cpp
+++ IA.cpp
@@ -7,6 +7,57 @@
 #include <algorithm>
 #include <map>
 
+
+
+std::vector<std::pair<int,int>  > sorted_crews(std::vector<int> trip, std::vector<int> caps){
+    std::vector<std::pair<int, int> > vp; 
+    std::vector<int> disponibilidad;
+    std::transform(caps.begin(), caps.end(), trip.begin(), std::back_inserter(disponibilidad), std::minus<int>());
+
+    // Inserting element in pair vector 
+    // to keep track of previous indexes 
+    for (int i = 0; i < disponibilidad.size(); ++i) { 
+        vp.push_back(std::make_pair(disponibilidad[i], i)); 
+    } 
+  
+    // Sorting pair vector 
+    std::sort(vp.begin(), vp.end(), std::greater<std::pair<int,int>>());
+    for (auto i: vp){
+        std::cout << i.first << "," << i.second << "\n";
+    }
+    return vp;
+}
+
+/*
+void sortArr(int arr[], int n) 
+{ 
+  
+    // Vector to store element 
+    // with respective present index 
+    vector<pair<int, int> > vp; 
+  
+    // Inserting element in pair vector 
+    // to keep track of previous indexes 
+    for (int i = 0; i < n; ++i) { 
+        vp.push_back(make_pair(arr[i], i)); 
+    } 
+  
+    // Sorting pair vector 
+    sort(vp.begin(), vp.end()); 
+  
+    // Displaying sorted element 
+    // with previous indexes 
+    // corresponding to each element 
+    cout << "Element\t"
+         << "index" << endl; 
+    for (int i = 0; i < vp.size(); i++) { 
+        cout << vp[i].first << "\t"
+             << vp[i].second << endl; 
+    } 
+} */
+
+
+
 template <class Container>
 void split2(const std::string& str, Container& cont, char delim = ';')
 {
@@ -29,7 +80,7 @@ void split3(const std::string& str, Container& cont, char delim = ',')
 
 int main(){
 
-
+    std::vector <std::vector<int>> matrix;
     std::ifstream infile("./Instancias PPP/Instancias CSPLib/Ian01.txt");
     std::string mytext;
     int yates;
@@ -64,9 +115,13 @@ int main(){
         }
         i++;
     }
-    for (auto i: tripulaciones){
-        std::cout << i << "\n";
-    }
+
+    /*
+    vector<vector<int>> matrix();
+    for ( int i = 0 ; i < RR ; i++ ){
+        matrix[i].resize(CC);
+    }*/
+    sorted_crews(tripulaciones,capacidades);
     /*std::cout << yates << "\n";
     std::cout << periodos << "\n";*/
     return 0;
