7a31cca45197b442b7ffa58abdc61ada44ac7c9a
Felipitoo <felipe.montero.14@sansano.usm.cl>
2021-01-02 04:00:12 -0300

    visitados

--- IA.cpp
+++ IA.cpp
@@ -9,6 +9,27 @@
 
 using namespace std;
 
+
+
+void print_matrix(vector<vector<int>> matrix){
+    for(int i = 0 ; i < matrix.size(); i++){
+        for(int j = 0 ; j< matrix[0].size();j++){
+            cout << matrix[i][j] << " ";
+        }
+        cout << "\n";
+    }
+}
+
+
+void print_vector(vector<int> vect){
+    for(int i = 0 ; i < vect.size(); i++){
+         cout << vect[i] << " ";
+        cout << "\n";
+    }
+}
+
+
+
 std::vector<std::pair<int,int>> sorted_crews(std::vector<int> trip, std::vector<int> caps){
     std::vector<std::pair<int, int> > vp; 
     std::vector<int> disponibilidad;
@@ -87,26 +108,109 @@ int check_ya_visito_host (vector<vector<int>> visitas, int visitante, int host){
     }
 }
 
-void actualizar_visitados(vector<vector<int>> visitas, int periodo, int yates){
+// revisa si un host visito a alguien siendo host, retorna 1 si rompe esto, retorna 0 en el caso contrario
+ int check_es_host (vector<vector<int>> solucion , vector<int> host_periodo, int periodo){
+     int contador = 0;
+     for(int i = 0; i< host_periodo.size(); i++){
+         if( (solucion[i][periodo] != -1) && (host_periodo[i] == 1) ){
+             contador++;
+         }
+     }
+     return contador;
+
+ }
 
 
-}
-
-
-void print_matrix(vector<vector<int>> matrix){
-    for(int i = 0 ; i < matrix.size(); i++){
-        for(int j = 0 ; j< matrix[0].size();j++){
-            cout << matrix[i][j] << " ";
-        }
-        cout << "\n";
+int determinar_min (vector<int> tripulaciones, int periodos, vector<pair<int,int>> disponibilidades_or){
+    int total = 0;
+    int pueden = 0;
+    for (int i = 0; i < tripulaciones.size(); i++){
+        total = total + tripulaciones[i];
     }
+    int min_yates = 0;
+    int j;
+     while (j < disponibilidades_or.size()){
+        pueden = pueden + disponibilidades_or[j].first;
+        min_yates++;
+        if(pueden > total){
+            j = j + disponibilidades_or.size();
+        }
+        j++;
+    }
+
+    if (periodos < min_yates){
+
+
+        return min_yates;
+    }
+    else{
+
+        return periodos;
+    }
+
+
+
 }
+
+
+map<int, vector<int>> init_relaciones(int yates){
+    map<int, vector<int>> aux;
+    for (int i = 0; i < yates; i++){
+        aux[i] = {};
+    }
+    return aux;
+}
+
+vector<vector<int>> actualizar_visitados(vector<vector<int>> solucion, int periodos, int yates){
+    vector<vector<int>> visitas;
+    visitas = init_visitados(yates);
+    int i,j,k,y,z;
+    
+
+    for(j = 0 ; j < periodos; j++){
+        for(i = 0; i < yates ; i++){
+            vector<int> encuentros;
+            bool flag = false;
+            for(k = 0; k < yates; k++){
+                if(solucion[k][j] == i ){
+                    flag = true;
+                    encuentros.push_back(k);
+                }
+            }
+            if(flag){
+                encuentros.push_back(i);
+            }
+          
+            for(auto fila : encuentros){
+                for(auto columna : encuentros){
+                    if( fila != columna){
+                        visitas[fila][columna]++;
+                    }
+                }
+            }
+        }
+
+    }
+    
+    return visitas;
+    
+
+}
+
+void actualizar_costos(vector<vector<int>> solucion, int periodos, int yates){
+    
+
+}
+
+
+
 
 // Genera solucion greedy que relaja restriccion de dos invitados no pueden volver a verse
+// La decision greedy es llenar siempre primero los barcos con mayor capacidad que no rompan el resto de las restricciones
 vector<vector<int>> greedy(vector<int> trip, vector<int> caps, int periodos, int yate){
     
     vector<pair<int,int>> ordenadas;
-    ordenadas = sorted_crews_ascending(trip,caps);
+    ordenadas = sorted_crews(trip,caps);
 
     int invitados = trip.size();
     vector<vector<int>> visitados(invitados);
@@ -119,13 +223,14 @@ vector<vector<int>> greedy(vector<int> trip, vector<int> caps, int periodos, int
         solucion_greedy[i].resize(periodos,-1);
     }
 
-
+    vector<vector<int>> host_periodos(periodos); 
     
     for(int j = 0; j < periodos; j++){
         vector<int> hosts(yates);
         vector<int> disponibilidad;
         std::transform(caps.begin(), caps.end(), trip.begin(), std::back_inserter(disponibilidad), std::minus<int>());
         for(int i = 0; i < yates; i++){
+            if (!hosts[i]){
                 int host_candidato = 0;
                 while(host_candidato < yates){
                     int cumplidas = 2;
@@ -143,12 +248,27 @@ vector<vector<int>> greedy(vector<int> trip, vector<int> caps, int periodos, int
                     host_candidato++;
 
                 }
-              
+            }
+        }
+        host_periodos[j] = hosts;
+    }
+
+    for (int j = 0; j < periodos ; j++){
+        for(int i = 0; i < yates ; i++){
+            if (host_periodos[j][i] == 1){
+                solucion_greedy[i][j] = -1;
+            }
         }
     }
     return solucion_greedy;
 }
 
+int funcion_costos(vector<vector<int>> solucion_candidata){
+    int costo;
+    
+    return costo;
+
+}
 
 
 void greedy_HC_peque(vector<int> trip, vector<int> caps, int periodos, int yates){
@@ -159,10 +279,14 @@ void greedy_HC_peque(vector<int> trip, vector<int> caps, int periodos, int yates
         matrix[i].resize(periodos);
     }
 
+    vector<pair<int,int>> ordenados; 
+    
     vector<vector<int>> solucion_candidata;
     vector<vector<int>> sol_inicial(invitados);
     solucion_candidata = greedy(trip,caps,periodos,yates);
     print_matrix(solucion_candidata);
+ 
+    actualizar_visitados(solucion_candidata,periodos,yates);
 
 
 
